cbcb79c77ed77e6144a7fdf5a551dc2a1cf87640
Kyle - GF63 <kyle.volle@gmail.com>
2020-10-02 06:37:31 -0500

    I believe I've fixed the data augmentation issue

--- data.py
+++ data.py
@@ -8,7 +8,9 @@ def data_source(directory):
         directory,
         target_size=(480, 640),
         batch_size=32,
+        shuffle=True,
         class_mode='input')
+
     return generator
 
 def augment(image):
@@ -24,13 +26,14 @@ def augment(image):
 
 @tf.function
 def augment_image(image):
+    batch = tf.shape(image)[0]
     if tf.random.uniform(()) > 0.5:
         image = tf.image.flip_left_right(image)
-    image = tf.image.random_brightness(image, 0.125)
-    #if tf.random.uniform(()) > 0.9:
-    #    image = tf.image.rgb_to_grayscale(image)
+    image = tf.image.random_brightness(image, 0.25)
+    ##if tf.random.uniform(()) > 0.9:
+    ##    image = tf.image.rgb_to_grayscale(image)
     sf = .2*tf.random.uniform(())+0.9
-    image = tf.image.random_crop(image, [1,sf*360, sf*480, 3])
+    image = tf.image.random_crop(image, [batch, sf*360, sf*480, 3])
     image = tf.image.resize(image, [ 360, 480])
-    #return tf.image.central_crop(image, (224, 224))
+    ##return tf.image.central_crop(image, (224, 224))
     return tf.image.crop_to_bounding_box(image, 68, 128, 224, 224)
