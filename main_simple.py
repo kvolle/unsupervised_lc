53aa2635a2a249c0a5ab2f9233221e24f1819065
Kyle - GF63 <kyle.volle@gmail.com>
2020-10-02 10:23:24 -0500

    Hopefully working

--- main_simple.py
+++ main_simple.py
@@ -7,6 +7,8 @@ from tensorflow.keras import layers
 from tensorflow.keras import models
 from tensorflow.keras import optimizers
 from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras import backend as K
+
 # Move these to new file
 def cosine_distance(vects):
     # is a distance, so taking compliment of similarity
@@ -19,16 +21,19 @@ def eucl_dist_output_shape(shapes):
     return (shape1[0], 1)
 
 
-def clr_loss(vects):
-    x, y = vects
-    batch, feats = x.shape
+#def clr_loss(vects):
+def distance_loss(y_true, y_pred):
+    #x, y = vects
+    x, y = tf.split(y_pred, 2, 1)
+    batch = tf.shape(x)[0]
+    if (batch == None):
+        batch = 3
     dots = tf.matmul(x, y, transpose_b=True)
     identity = tf.eye(batch)
     comp = tf.linalg.band_part(tf.ones(shape=(batch, batch)), 2, 2) - identity
-    true = tf.matmul(identity, dots)
-    false = comp - tf.matmul(comp, dots)
-    print(true.shape)
-    return true + false
+    true = identity - tf.matmul(identity, dots)
+    false = tf.matmul(comp, dots)
+    return K.sum(true) + K.sum(false)
 
 # Define data source and declare input
 train = data.data_source("./Data")
@@ -43,28 +48,29 @@ net = local_models.simple()
 latent_one = net(augmented_one)
 latent_two = net(augmented_two)
 
+encodings = layers.concatenate([latent_one, latent_two], axis=1)
 # Calculate the distance between outputs
 #distance =  tf.keras.layers.Lambda(cosine_distance,
 #                  output_shape=eucl_dist_output_shape)([latent_one, latent_two])
 #distance2 = tf.keras.layers.Lambda(cosine_distance,
 #                  output_shape=eucl_dist_output_shape)([latent_one, tf.random.shuffle(latent_two)])
-distance = tf.keras.layers.Lambda(clr_loss,
-                  output_shape=eucl_dist_output_shape)([latent_one, latent_two])
+#distance = tf.keras.layers.Lambda(clr_loss,
+#                  output_shape=eucl_dist_output_shape)([latent_one, latent_two])
 
 # Declare the model
-model = models.Model(inputs=[input_img], outputs=[distance, latent_one, latent_two])
+model = models.Model(inputs=[input_img], outputs=[encodings, latent_one, latent_two])
 
 # Print model summary
-#model.summary()
+model.summary()
 #tf.keras.utils.plot_model(model, "test.png")
 
-# Define the loss function
-# (This should also be pulled to new file)
-def distance_loss(y_true, y_pred):
-    return tf.keras.backend.sum(tf.keras.backend.abs(y_pred))
+## Define the loss function
+## (This should also be pulled to new file)
+#def distance_loss(y_true, y_pred):
+#    return tf.keras.backend.sum(tf.keras.backend.abs(y_pred))
 
 # Compile the model
-model.compile(optimizer=optimizers.Adam(), loss=[distance_loss])
+model.compile(optimizer=optimizers.Adam(), loss=[distance_loss, None, None])
 #model.compile(optimizer=optimizers.Adam(), loss=[MeanSquaredError, MeanSquaredError, distance_loss])
 
 # Model won't train, so predict
@@ -72,7 +78,7 @@ model.compile(optimizer=optimizers.Adam(), loss=[distance_loss])
 #z = model.predict(x)
 
 # Train the model
-EPOCHS = 5
+EPOCHS = 25
 model.fit(train, epochs=EPOCHS)
 
 _, out1, out2 = model.predict(train)
